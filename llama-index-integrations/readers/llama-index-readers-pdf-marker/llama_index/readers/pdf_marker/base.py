f62f89a1b9ca51fdf84018e60cccdcae9d21ba72
Nathan Lenas <nathan.lenas@lmddc.lu>
2024-10-29 14:08:40 +0100

    Avoid failing when a document cannot be read

--- llama-index-integrations/readers/llama-index-readers-pdf-marker/llama_index/readers/pdf_marker/base.py
+++ llama-index-integrations/readers/llama-index-readers-pdf-marker/llama_index/readers/pdf_marker/base.py
@@ -39,60 +39,68 @@ class PDFMarkerReader(BaseReader):
         from marker.models import load_all_models
         from pypdf import PdfReader, PdfWriter
 
-        pdf = PdfReader(str(file))
+        try:
+            pdf = PdfReader(str(file))
 
-        inferred_langs = []
-        if langs is None:
-            if "EN" in file.name:
-                inferred_langs.append("English")
-            if "DE" in file.name:
-                inferred_langs.append("German")
-            if "FR" in file.name:
-                inferred_langs.append("French")
-            if "LU" in file.name:
-                inferred_langs.append("English")
-                inferred_langs.append("French")
-                inferred_langs.append("German")
-        #
-        pages = []
-        num_pages = len(pdf.pages)
-        # create a new pdf for each page
-        for i in range(num_pages):
-            writer = PdfWriter()
-            writer.add_page(pdf.pages[i])
-            page_path = str(file).replace(".pdf", f"_{i}.pdf")
-            pages.append({"path": page_path, "page_number": i + 1})
-            writer.write(page_path)
+            inferred_langs = []
+            if langs is None:
+                if "EN" in file.name:
+                    inferred_langs.append("English")
+                if "DE" in file.name:
+                    inferred_langs.append("German")
+                if "FR" in file.name:
+                    inferred_langs.append("French")
+                if "LU" in file.name:
+                    inferred_langs.append("English")
+                    inferred_langs.append("French")
+                    inferred_langs.append("German")
+            #
+            pages = []
+            num_pages = len(pdf.pages)
+            # create a new pdf for each page
+            for i in range(num_pages):
+                writer = PdfWriter()
+                writer.add_page(pdf.pages[i])
+                page_path = str(file).replace(".pdf", f"_{i}.pdf")
+                pages.append({"path": page_path, "page_number": i + 1})
+                writer.write(page_path)
 
-        model_lst = load_all_models()
+            model_lst = load_all_models()
 
-        docs = []
-        for p in pages:
-            try:
-                full_text, images, out_meta = convert_single_pdf(
-                    p["path"],
-                    model_lst,
-                    max_pages=max_pages,
-                    langs=langs if langs else inferred_langs,
-                    batch_multiplier=batch_multiplier,
-                    start_page=start_page,
-                )
-                if extra_info:
-                    extra_info["title"] = Path(p["path"]).name
-                    extra_info["page_number"] = p["page_number"]
-                    docs.append(Document(text=full_text, extra_info=extra_info))
-                else:
-                    extra_info = {
-                        "title": p["path"].split("/")[-1],
-                        "page_number": p["page_number"],
-                    }
-            except Exception as e:
-                # If an error occurs, continue to the next page
-                # print(f"Error in processing page {p['page_number']}: {e}")
-                continue
+            docs = []
+            for p in pages:
+                try:
+                    full_text, _, _ = convert_single_pdf(
+                        p["path"],
+                        model_lst,
+                        max_pages=max_pages,
+                        langs=langs if langs else inferred_langs,
+                        batch_multiplier=batch_multiplier,
+                        start_page=start_page,
+                    )
+                    if extra_info:
+                        extra_info["title"] = Path(p["path"]).name
+                        extra_info["page_number"] = p["page_number"]
+                        docs.append(Document(text=full_text, extra_info=extra_info))
+                    else:
+                        extra_info = {
+                            "title": p["path"].split("/")[-1],
+                            "page_number": p["page_number"],
+                        }
+                except Exception as e:
+                    # If an error occurs, continue to the next page
+                    print(f"Error in processing page {p['page_number']}: {e}")
+                    continue
 
-        # remove the temporary files
-        for p in pages:
-            Path(p["path"]).unlink()
+            # remove the temporary files
+            for p in pages:
+                try:
+                    Path(p["path"]).unlink()
+                except:
+                    continue
+            return docs
+            
+        except Exception as e:
+            print("Error processing " + str(file) +" : " + str(e))
+            return []
 
-        return docs
